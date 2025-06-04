634865085e7ef8ce2ca65742d605048a06097d5c
Vygantas Butkus <vygantas.butkus@gmail.com>
2025-06-04 23:42:16 +0300

    Client api skeleton

--- src/anre/client/poly/api/api_client.py
+++ src/anre/client/poly/api/api_client.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Literal
 from typing import overload, Union
 
 
@@ -12,12 +12,13 @@ from py_clob_client.clob_types import (
     BalanceAllowanceParams,
     OrderArgs,
     OrderType,
+    OpenOrderParams,
 )
 from py_clob_client.constants import POLYGON
-from py_clob_client.order_builder.constants import BUY
+from py_clob_client.order_builder.constants import BUY,SELL
 
 from anre.config.config import config as anre_config
-
+from anre.client.poly.api.gamma import GammaMarketClient
 
 
 class ApiClient:
@@ -28,6 +29,7 @@ class ApiClient:
             self._clob_client_cls = self._create_clob_client_cls()
         assert isinstance(self._clob_client_cls, ClobClient)
         self._clob_client: ClobClient = self._clob_client_cls
+        self.gamma = GammaMarketClient()
 
     def _create_clob_client_cls(self):
         polymarket_creds = anre_config.cred.get_polymarket_creds()
@@ -50,6 +52,9 @@ class ApiClient:
     def cancel_all(self):
         self._clob_client.cancel_all()
 
+    def get_clob_market(self, condition_id: str):
+        return self._clob_client.get_market(condition_id=condition_id)
+
     def get_clob_markets(self, next_cursor: str = "MA=="):
         return self._clob_client.get_markets(next_cursor=next_cursor)
 
@@ -62,8 +67,23 @@ class ApiClient:
     def get_sampling_simplified_markets(self, next_cursor: str = "MA=="):
         return self._clob_client.get_sampling_simplified_markets(next_cursor=next_cursor)
 
-    def get_pr(self):
-        return self._clob_client.get_prices()
+    def get_orders(self, id: str = None, market: str = None, asset_id: str = None, next_cursor: str = "MA=="):
+        return self._clob_client.get_orders(OpenOrderParams(id=id, market=market, asset_id=asset_id), next_cursor=next_cursor)
+
+    def get_order(self, order_id: str = None):
+        return self._clob_client.get_order(order_id=order_id)
+
+    def place_order(self, token_id: str, price: float, size: float, side: Literal["BUY", "SELL"], order_type: str = "GTC"):
+        order_args = OrderArgs(
+            price=price,
+            size=size,
+            side=side,
+            token_id=token_id,
+        )
+        signed_order = self._clob_client.create_order(order_args)
+        assert order_type in OrderType.__dict__
+        resp = self._clob_client.post_order(signed_order, order_type)
+        return resp
 
 
 
@@ -82,3 +102,23 @@ def __dummy__():
 
     markets = self.get_sampling_simplified_markets()
     market_dict = markets['data'][0]
+
+    self.gamma.get_events(slug="what-will-powell-say-during-june-press-conference")
+    self.gamma.get_markets(slug="will-powell-say-inflation-40-times-during-june-press-conference")
+    market = self.gamma.get_market('542930')
+    condition_id = market['conditionId']
+    market = self.get_clob_market(condition_id=condition_id)
+    token_id = market['tokens'][0]['token_id']
+
+
+    place_resp = self.place_order(
+        token_id=token_id,
+        price=0.1,
+        size=100,
+        side=BUY,
+    )
+
+    order = self.get_order(order_id=place_resp['orderID'])
+
+    self.get_orders()
+    self.cancel_all()
